815af3c74591da5e2c8b8766e37419a6fa50112c
luisgrimaldi <100171171+luisgrimaldi@users.noreply.github.com>
2023-06-21 02:23:09 -0700

    Update 21_06_23 Exceptions.py

--- Exceptions.py
+++ Exceptions.py
@@ -1,26 +1,26 @@
 import time
-def saveError(response):
+def saveError(response, cliente):
     txt = "Código de error " + str(response.status_code) + " " + response.reason + "\n"
     txt += "Response: \n" + response.content.decode() + "\n\n"
     txt += "Body: \n" + response.request.body
 
-    fileName = "error " + str(round(time.time()*1000)) + ".txt" 
+    fileName = "Mensajes/error " + cliente + " " +str(round(time.time()*1000)) + ".txt" 
     f = open(fileName, "a")
     f.write(txt)
     f.close()
 
-def saveOK(response):
+def saveOK(response, cliente):
     ##txt = "Código de error " + str(response.status_code) + " " + response.reason + "\n"
     txt = "Response: \n" + response.content.decode() + "\n\n"
     txt += "Body: \n" + response.request.body
 
-    fileName = "OK " + str(round(time.time()*1000)) + ".txt" 
+    fileName = "Mensajes/OK " + cliente + " " + str(round(time.time()*1000)) + ".txt" 
     f = open(fileName, "a")
     f.write(txt)
     f.close()
 
-def saveExcept(text):
-    fileName = "Exception " + str(round(time.time()*1000)) + ".txt" 
+def saveExcept(text, cliente):
+    fileName = "Mensajes/Exception " + cliente + " " + str(round(time.time()*1000)) + ".txt" 
     f = open(fileName, "a")
     f.write(text)
     f.close()
