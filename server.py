aa1a0103e97d7a0e8177187c968fdfd0cac4c231
luisgrimaldi <100171171+luisgrimaldi@users.noreply.github.com>
2023-06-23 02:02:27 -0700

    Update 23_06_23  server.py

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 import threading
 import socket
+import Exceptions as Ex
 import Retranslator as R
 
 CONNECTION = (socket.gethostname(), 80)
@@ -11,34 +12,37 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 server.bind((socket.gethostname(), 80))
-server.listen(15)
+server.listen(100)
 
 
 def clientthread(conn):
 
-    serialNumber = ""
-    while True:
-        #Recibe datos del cliente
-        data = conn.recv(65495)
-        if not data:
-            break
-        #print("\n"+str(serialNumber)+ " package:    " + data.decode() + "\n\n")
-        serialNumber = R.retranslate(data.decode(), serialNumber)
+	serialNumber = ""
+	while True:
+    
+		try:
+			#Recibe datos del cliente
+			data = conn.recv(65495)
+			if not data:
+				break
+			#print(data)#print("\n"+str(serialNumber)+ " package:    " + data.decode() + "\n\n")
+			serialNumber = R.retranslate(data.decode(), serialNumber)
          
             
-        # el paquete fue recibido con exito
-        conn.send("OK".encode())
-        #test
-    conn.close()
+			# el paquete fue recibido con exito
+			conn.send("OK".encode())
+		except Exception as e:
+			Ex.saveExcept("OS error: {0}".format(e), "SERVER")
+	conn.close()
 
 while True:
 
-    print ("Listen {0} on {1}".format(*CONNECTION))
-    sck, addr = server.accept()
-    print ("Connected {0}:{1}".format(*addr))
-    t = threading.Thread(target=clientthread, args=(sck,))
-    t.daemon = True
-    t.start()
+	print ("Listen {0} on {1}".format(*CONNECTION))
+	sck, addr = server.accept()
+	print ("Connected {0}:{1}".format(*addr))
+	t = threading.Thread(target=clientthread, args=(sck,))
+	t.daemon = True
+	t.start()
 
 #conn.close()
 sck.close()
